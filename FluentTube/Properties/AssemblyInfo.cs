5a9a74f03d8dfdd79a29a92688571b8292275468
onein528 <onein528@outlook.com>
2022-06-22 00:01:03 +0900

    Initialized repository

---
+++ FluentTube/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FluentTube")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FluentTube")]
+[assembly: AssemblyCopyright("Copyright © 2022 Tomoyuki Terashita")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
---
+++ YouTubeApi/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace YouTubeApi
+{
+    public class Class1
+    {
+
+    }
+}
